cb675e31d6d94f26840ed21ba7a1d3b89870df12
Roshni <162465526+RoshniRparmar@users.noreply.github.com>
2025-06-24 10:25:46 +0530

    Create App.tsx

---
+++ Replit-send supplier/App.tsx
@@ -0,0 +1,38 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Layout from "@/components/layout";
+import Home from "@/pages/home";
+import SupplierRegistration from "@/pages/supplier-registration";
+import Suppliers from "@/pages/suppliers";
+import PlaceOrder from "@/pages/place-order";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/register" component={SupplierRegistration} />
+        <Route path="/suppliers" component={Suppliers} />
+        <Route path="/orders/new" component={PlaceOrder} />
+        <Route component={NotFound} />
+      </Switch>
+    </Layout>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
